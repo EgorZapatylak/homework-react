9c0ca604c1f4ca58f9c2e7ecd6038b768df920fa
EgorZapatylak <zapatylak96a@gmail.com>
2022-04-21 15:21:33 +0300

    Created Junior.tsx page and add some props

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Junior() {
+    return (
+        <div>
+            {/*<HW7/>*/}
+            {/*<HW8/>*/}
+            {/*<HW9/>*/}
+            {/*<HW10/>*/}
+            {/*<HW11/>*/}
+            <p>Junior task</p>
+
+        </div>
+    )
+}
+
+export default Junior
