bb8ce0801003adcd4a8721cd5239ec0896f29ae3
EgorZapatylak <zapatylak96a@gmail.com>
2022-04-21 15:21:55 +0300

    Created Junior_plus.tsx page and add some props

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior_plus.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Junior_plus() {
+    return (
+        <div>
+            {/*<HW12/>*/}
+
+            <p>Junior plus task</p>
+
+        </div>
+    )
+}
+
+export default Junior_plus
