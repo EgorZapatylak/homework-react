cbb9b64014f687d51ab1dc22e01593ed672092a9
EgorZapatylak <zapatylak96a@gmail.com>
2022-04-21 17:16:20 +0300

    Added CSS module in file. Create Navbar menu

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            <NavLink to={'/pre-junior'}>Pre-junior</NavLink>---
-            <NavLink to={'/junior'}>Junior</NavLink>---
-            <NavLink to={'/junior-plus'}>Junior plus</NavLink>---
+        <div className={s.menu}>
+            <NavLink className={s.menu_item} to={'/pre-junior'}>Pre-junior</NavLink>
+            <NavLink className={s.menu_item} to={'/junior'}>Junior</NavLink>
+            <NavLink className={s.menu_item} to={'/junior-plus'}>Junior plus</NavLink>
             {/*// add NavLinks*/}
-
         </div>
     )
 }
