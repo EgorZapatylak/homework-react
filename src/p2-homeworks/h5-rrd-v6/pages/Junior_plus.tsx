4ff96c5d3c133426d720a5ffb1f5324af2714bc9
EgorZapatylak <zapatylak96a@gmail.com>
2022-04-21 17:13:23 +0300

    Added CSS module in file Junior_plus.tsx

--- src/p2-homeworks/h5-rrd-v6/pages/Junior_plus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior_plus.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from './Junior_plus.module.css'
 
 function Junior_plus() {
     return (
-        <div>
+        <div className={s.body}>
             {/*<HW12/>*/}
 
             <p>Junior plus task</p>
