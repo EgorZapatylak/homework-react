6d8ac86335dc4580f649c40c77d8a12912da182c
EgorZapatylak <zapatylak96a@gmail.com>
2022-04-21 15:26:31 +0300

    Import props in Pages.tsx file

---
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Error404 from "./pages/Error404";
+import PreJunior from "./pages/PreJunior";
+import Junior from "./pages/Junior";
+import Junior_plus from "./pages/Junior_plus";
+import {Routes, Route, Navigate} from "react-router-dom";
+
+export const PATH = {
+    PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus',
+}
+
+function Pages() {
+    return (
+        <div>
+            {/*Routes выбирает первый подходящий роут*/}
+            <Routes>
+
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<Junior_plus/>}/>
+
+                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                <Route path={'/*'} element={<Error404/>}/>
+
+            </Routes>
+        </div>
+    )
+}
+
+export default Pages
